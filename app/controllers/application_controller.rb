fda27db9b5c7307cdf3f79cb5ee9ad1c4d60dcf8
Daniel Nolan <danielanolan@gmail.com>
2024-06-09 19:44:40 -0400

    Use assistant name as header and page title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  helper_method :assistant_name
+  def assistant_name
+    assistant_id = Rails.application.credentials.open_ai.dig("assistant_id")
+    assistant = client.assistants.retrieve(id: assistant_id)
+    assistant["name"]
+  end
 end
