281319e64a45e6f2c1baaa6be730eceb691853bb
Daniel Nolan <danielanolan@gmail.com>
2025-05-16 16:47:13 +0000

    Fix double form submit and update icons

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,9 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form"
 export default class extends Controller {
+  static targets = ["disableable"]
   connect() {
+    addEventListener("turbo:submit-start", ((event) => {
+      this.disableableTarget.disable();
+    }))
     addEventListener("turbo:submit-end", ((event) => {
       this.reset();
+      this.disableableTarget.enable();
     }))
   }
 
