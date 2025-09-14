b59d39b0d5a05aaa5142330b4dae9e3fe9c4a394
Daniel Nolan <danielanolan@gmail.com>
2025-09-14 13:37:58 +0000

    WIP clean up class

--- app/services/response_creator.rb
+++ app/services/response_creator.rb
@@ -1,76 +1,63 @@
 class ResponseCreator
   def create_response(message:)
-    input = [{role: "user", content: message.content }]
+    input = [{role: "user", content: message.content}]
     stream = client.responses.stream(
       input: input,
-      prompt: {
-        id: ENV["OPENAI_PROMPT_ID"],
-      },
+      prompt: {id: ENV["OPENAI_PROMPT_ID"]}
     )
+    stream_and_broadcast(stream:, message:)
 
-    content = ""
-    stream.each do |event|
-      puts ("--- events from first stream ---")
-      puts event
-      case event
-      when OpenAI::Streaming::ResponseTextDeltaEvent
-        content << event.delta
-        assistant_message = Message.new(id: message.id, content:, role: "assistant")
-        broadcast_response(assistant_message)
-      end
-    end
     response = stream.get_final_response
     input += response.output
 
-    response
-    .output
-    .each do |output|
-      case output
-      when OpenAI::Models::Responses::ResponseFunctionToolCall
-        tool_output = tools.public_send(output.name, output.arguments)
-        input.push({
-          type: :"function_call_output",
-          call_id: output.call_id,
-          output: tool_output
-        })
-      end
-    end
+    input_with_tool_calls = handle_tool_calls(input:, outputs: response.output)
 
     stream_with_tool_output = client.responses.stream(
-      input: input,
-      prompt: {
-        id: ENV["OPENAI_PROMPT_ID"],
-      },
+      input: input_with_tool_calls,
+      prompt: {id: ENV["OPENAI_PROMPT_ID"]}
     )
-
-    content = ""
-    stream_with_tool_output.each do |event|
-      puts ("--- events from second stream ---")
-      puts event
-      case event
-      when OpenAI::Streaming::ResponseTextDeltaEvent
-        content << event.delta
-        assistant_message = Message.new(id: message.id, content:, role: "assistant")
-        broadcast_response(assistant_message)
-      end
-    end
+    stream_and_broadcast(stream: stream_with_tool_output, message:)
   end
 
   private
 
-  def client
-    @client ||= OpenAI::Client.new
+  def handle_tool_calls(input:, outputs:)
+    outputs.each do |output|
+      if output.is_a?(OpenAI::Models::Responses::ResponseFunctionToolCall)
+        tool_output = tools.public_send(output.name, output.arguments)
+        input << {
+          type: :function_call_output,
+          call_id: output.call_id,
+          output: tool_output
+        }
+      end
+    end
+    input
   end
 
-  def additional_instructions
-    "The current date and time is #{DateTime.current}."
+  def stream_and_broadcast(stream:, message:)
+    content = ""
+    stream.each do |event|
+      puts("--- events from stream ---")
+      puts event
+      if event.is_a?(OpenAI::Streaming::ResponseTextDeltaEvent)
+        content << event.delta
+        assistant_message = Message.new(id: message.id, content:, role: "assistant")
+        broadcast_response(assistant_message)
+      end
+    end
+    content
+  end
+
+  def client
+    @client ||= OpenAI::Client.new
   end
 
   def broadcast_response(assistant_message)
     Turbo::StreamsChannel.broadcast_render_to(
       :messages,
       partial: "messages/assistant_message",
-      locals: { message: assistant_message }
+      locals: {message: assistant_message}
     )
   end
 
@@ -78,4 +65,3 @@ class ResponseCreator
     @tools ||= Tools.new
   end
 end
-
